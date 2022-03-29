0bd51f782b727030f784d04cf77d96ae3eaf509b
luankefei <luankefei@gmail.com>
2022-03-29 14:19:58 +0800

    增加圣遗物浮层

--- src/components/ArtifactModal/ArtifactModal.tsx
+++ src/components/ArtifactModal/ArtifactModal.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState, useContext } from "react";
+import Modal from "../../components/Modal";
 import { Container, WeaponModalDetail, WeaponList } from "./artifact-modal.style";
 
 import { ICharacter, IWeaponData } from "../../interface/genshin.type";
@@ -9,7 +11,39 @@ type IProps = {
 };
 
 const ArtifactModal = (props: IProps) => {
-  return <div>1234</div>;
+  const { isOpen, character, onClose } = props;
+  const [visible, setVisible] = useState(isOpen);
+
+  const onModalClose = () => {
+    // setElementFilter("");
+    // setModalCharacter("");
+    onClose();
+  };
+
+  const renderArtifactList = () => {
+    return <li>1234</li>;
+  };
+
+  return (
+    <Modal visible={visible} onClose={onModalClose}>
+      <Container>
+        <WeaponModalDetail>
+          <div
+            className="header"
+            style={{
+              backgroundImage: `url(https://seelie.inmagi.com/img/characters/bg/${character && character.enName}.png)`,
+            }}
+          >
+            <img className="icon" src="/weapons/kaguras-verity/icon" alt="kaguras-verity" />
+            <span>神乐之真意</span>
+          </div>
+          <div>
+            <WeaponList>{renderArtifactList()}</WeaponList>
+          </div>
+        </WeaponModalDetail>
+      </Container>
+    </Modal>
+  );
 };
 
 export default ArtifactModal;
